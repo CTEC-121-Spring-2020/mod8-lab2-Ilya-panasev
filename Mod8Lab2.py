56e8b0306619101211c134797f9a93a20122b7df
Ilya <i.panasevich@students.clark.edu>
2020-06-10 12:23:15 -0700

    class demos

---
+++ Mod8Lab2.py
@@ -0,0 +1,17 @@
+"""
+CTEC 121
+Ilya Panasevich
+Module 8 Lab 2
+class demos
+"""
+
+""" IPO template
+Input(s): list/description
+Process: description of what function does
+Output: return value and description
+"""
+
+def main():
+    # code here
+
+main()    
